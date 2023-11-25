d9ab442d75bbb300ef82aae889a3300d18ccfb60
Kostek <ksytnik73kwer@gmail.com>
2023-11-25 23:04:37 +0100

    A new optional prop was added to the SelectionOption that allows adding a subtitle and styles to them.

--- front/src/components/SelectionOption/SelectionOption.tsx
+++ front/src/components/SelectionOption/SelectionOption.tsx
@@ -4,10 +4,11 @@ import {modelProps} from "../../App.tsx"
 type SelectionOptionProps = {
     text: string,
     is_selected: boolean,
-    onClick: (model: modelProps) => void
+    onClick: (model: modelProps) => void,
+    subtitle_text?: string,
 }
 
-function SelectionOption({text, is_selected, onClick}: SelectionOptionProps) {
+function SelectionOption({text, is_selected, onClick, subtitle_text}: SelectionOptionProps) {
     return (
         <div
             className={"selection-option"}
@@ -15,6 +16,7 @@ function SelectionOption({text, is_selected, onClick}: SelectionOptionProps) {
             <div className={"selection-option__left"}>
                 <div className={"selection-option__text"}>
                     {text}
+                    <div className={"subtitle-text"}>{subtitle_text}</div>
                 </div>
             </div>
             <div className={"selection-option__right"}>
