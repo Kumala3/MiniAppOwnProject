f3b93154944438f5c76159053bde1ff0a8ce97d1
Kostek <ksytnik73kwer@gmail.com>
2023-11-26 22:08:05 +0100

    Improve Switch component functionality and styles.

--- front/src/components/Switch/Switch.tsx
+++ front/src/components/Switch/Switch.tsx
@@ -1,10 +1,29 @@
 import "./Switch.scss"
+import classNames from "classnames"
+import {useState} from "react"
 
+type SwitchProps = {
+    Is_Checked: boolean,
+    set_Is_Checked: (is_checked: boolean) => void,
+}
 
-function Switch()
+function Switch({Is_Checked, set_Is_Checked}: SwitchProps)
 {
+    const [checked, setChecked] = useState(Is_Checked)
+
+    const toggleSwitch = () => {
+        setChecked(!checked)
+        set_Is_Checked(!checked)
+    }
+
     return (
-        <div></div>
+        <>
+            <span
+                className={classNames("switch", {"switch--checked": Is_Checked})}
+                onClick={toggleSwitch}>
+
+            </span>
+        </>
     )
 }
 
