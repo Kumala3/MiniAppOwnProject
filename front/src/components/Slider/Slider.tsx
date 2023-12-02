82e30289fe20169af60dd1024fec2c70637b7d51
Kostek <ksytnik73kwer@gmail.com>
2023-12-02 12:55:41 +0100

    Add min, max, and textUnit props to "Slider" component.

--- front/src/components/Slider/Slider.tsx
+++ front/src/components/Slider/Slider.tsx
@@ -2,14 +2,20 @@ import "./Slider.scss"
 
 
 type SliderProps = {
-
+    min: number,
+    max: number,
+    textUnit: string,
 }
 
-function Slider() {
+function Slider({min, max, textUnit}: SliderProps) {
 
     return (
         <div className={"slider"}>
-
+            <div className={"slider__top"}>
+                <div className={"slider__label"}>{min}</div>
+                <div className={"slider__value"}>{textUnit}</div>
+                <div className={"slider__label"}>{max}</div>
+            </div>
         </div>
     )
 }
