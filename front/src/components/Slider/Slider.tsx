b1941609289cb0a9f06d6ca75a4e328aa4529766
Kostek <ksytnik73kwer@gmail.com>
2023-12-02 13:56:29 +0100

    Update Slider component with enhanced functionalities and styles.

--- front/src/components/Slider/Slider.tsx
+++ front/src/components/Slider/Slider.tsx
@@ -1,21 +1,41 @@
 import "./Slider.scss"
+import React from "react"
 
 
 type SliderProps = {
     min: number,
     max: number,
     textUnit: string,
+    step: number,
+    currentValue: number,
+    setCurrentValue: (limit: number) => void,
 }
 
-function Slider({min, max, textUnit}: SliderProps) {
+function Slider({min, max, textUnit, step, currentValue, setCurrentValue}: SliderProps) {
+
+    const handleSave = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentValue(Number(event.target.value))
+    }
 
     return (
         <div className={"slider"}>
             <div className={"slider__top"}>
-                <div className={"slider__label"}>{min}</div>
-                <div className={"slider__value"}>{textUnit}</div>
-                <div className={"slider__label"}>{max}</div>
+                <div className={"slider__label"}>{min.toLocaleString()}</div>
+                <div className={"slider__value"}>
+                    {`${currentValue.toLocaleString()} ${textUnit}`}
+                </div>
+                <div className={"slider__label"}>{max.toLocaleString()}</div>
             </div>
+
+            <input
+                className={"slider__input"}
+                type={"range"}
+                min={min}
+                max={max}
+                value={currentValue}
+                step={step}
+                onChange={handleSave}
+            />
         </div>
     )
 }
