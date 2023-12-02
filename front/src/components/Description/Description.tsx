12b5ad9b443e6af9a1a933ca9fd02384a0ad17ce
Kostek <ksytnik73kwer@gmail.com>
2023-12-02 20:23:49 +0100

    Add fontWeight & color props to Description component for more effective using.

--- front/src/components/Description/Description.tsx
+++ front/src/components/Description/Description.tsx
@@ -1,14 +1,28 @@
 import "./Description.scss"
 import React from "react"
+import classNames from "classnames"
 
 type DescriptionProps = {
     text: string,
     children: React.ReactNode,
+    fontWeight?: "regular" | "semibold" | "bold",
+    color?: "hint" | "accent" | "link" | "text",
+
 }
 
-function Description({text, children}: DescriptionProps) {
+function Description({text, children, fontWeight, color}: DescriptionProps) {
     return (
-        <div className={"description"}>
+        <div
+            className={classNames("description",
+                {"font-weight-regular": fontWeight === "regular"},
+                {"font-weight-semibold": fontWeight === "semibold"},
+                {"font-weight-bold": fontWeight === "bold"},
+                {"tg-theme-text": color === "text"},
+                {"tg-theme-hint": color === "hint"},
+                {"tg-theme-link": color === "link"},
+                {"tg-theme-accent": color === "accent"},
+            )}
+        >
             {text}
             {children}
         </div>
