51828abebb9fd3962b1669714a3b7294c1beec7a
Kostek <ksytnik73kwer@gmail.com>
2023-12-03 20:54:46 +0100

    Add languages constant and type definition.

---
+++ front/src/constants/languages.ts
@@ -0,0 +1,11 @@
+export type Language =  {
+    code: string;
+    name: string;
+    localName: string;
+}
+
+export const LANGUAGES: Language[] = [
+    {code: "en", name: "English", localName: "English"},
+    {code: "ru", name: "Russian", localName: "Русский"},
+    {code: "uk", name: "Ukrainian", localName: "Українська"},
+]
