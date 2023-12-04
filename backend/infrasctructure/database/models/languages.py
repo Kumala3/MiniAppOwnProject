f40d1a6df79f0a9ddd7e063bd2a5a82ea2c412ba
Kostek <ksytnik73kwer@gmail.com>
2023-12-04 22:21:37 +0100

    Add Language class to database models.

---
+++ backend/infrasctructure/database/models/languages.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from sqlalchemy import BIGINT, VARCHAR, BOOLEAN, INTEGER, ForeignKey, false
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base, TimestampMixin, int_pk
+
+
+class Language(Base, TimestampMixin):
+    __tablename__ = 'languages'
+
+    language_id: Mapped[int_pk]
+    language_code: Mapped[str] = mapped_column(VARCHAR(7), unique=True)
+    local_name: Mapped[str] = mapped_column(VARCHAR(64))
+    language_name: Mapped[str] = mapped_column(VARCHAR(64))
