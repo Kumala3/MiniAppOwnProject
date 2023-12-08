dc229f1f91f4d0dd092260ffe6503d2c14e91999
Kostek <ksytnik73kwer@gmail.com>
2023-12-08 17:30:57 +0100

    Add database configuration class in backend.

---
+++ backend/config.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+from typing import Optional
+from sqlalchemy.engine.url import URL
+
+from environs import Env
+
+
+@dataclass
+class DbConfig:
+    """
+    Database configuration class.
+    This class holds the settings for the database, such as host, password, port, etc.
+
+    Attributes
+    ----------
+    host : str
+        The host where the database server is located.
+    password : str
+        The password used to authenticate with the database.
+    user : str
+        The username used to authenticate with the database.
+    database : str
+        The name of the database.
+    port : int
+        The port where the database server is listening.
+    """
+
+    host: str
+    password: str
+    user: str
+    database: str
+    port: int = 5432
+
+    # For SQLAlchemy
+    def construct_sqlalchemy_url(self, driver="asyncpg", host=None, port=None) -> str:
+        """
+        Constructs and returns a SQLAlchemy URL for this database configuration.
+        """
+
+        if not host:
+            host = self.host
+        if not port:
+            port = self.port
+        uri = URL.create(
+            drivername=f"postgresql+{driver}",
+            username=self.user,
+            password=self.password,
+            host=host,
+            port=port,
+            database=self.database,
+        )
+        return uri.render_as_string(hide_password=False)
+
+    @staticmethod
+    def from_env(env: Env):
+        """
+        Creates the DbConfig object from environment variables.
+        """
+        host = env.str("DB_HOST")
+        password = env.str("POSTGRES_PASSWORD")
+        user = env.str("POSTGRES_USER")
+        database = env.str("POSTGRES_DB")
+        port = env.int("DB_PORT", 5432)
+        return DbConfig(
+            host=host, password=password, user=user, database=database, port=port
+        )
