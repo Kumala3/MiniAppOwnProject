9d954d635019d7f9cd4fbc74e1e1acbb86bfc5c0
Kostek <ksytnik73kwer@gmail.com>
2023-12-09 13:59:00 +0100

    Add asynchronous DB engine and session pool setup.

---
+++ backend/infrastructure/database/setup.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from config import DbConfig
+
+
+def create_engine(db: DbConfig, echo=False):
+    engine = create_async_engine(
+        db.construct_sqlalchemy_url(),
+        query_cache_size=1200,
+        pool_size=20,
+        max_overflow=200,
+        echo=echo,
+    )
+    return engine
+
+
+def create_session_pool(engine):
+    session_pool = async_sessionmaker(bind=engine, expire_on_commit=False)
+    return session_pool
