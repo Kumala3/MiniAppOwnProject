3b7ce074e05709275a81aea73965ad097f22dc48
Kostek <ksytnik73kwer@gmail.com>
2023-12-10 20:51:21 +0100

    Update method in UserRepo to fetch more user info.

--- backend/infrastructure/database/repo/users.py
+++ backend/infrastructure/database/repo/users.py
@@ -5,9 +5,11 @@ from infrastructure.database.repo.base import BaseRepo
 
 
 class UserRepo(BaseRepo):
-    async def get_all_user_info(self, offset=None, limit=None) -> list[dict]:
+    async def get_user_info(self, offset=None, limit=None) -> list[dict]:
         stmt = select(User.user_id, User.username,
                       User.full_name, User.created_at,
-                      User.user_profile, User.response_profile)
+                      User.user_profile, User.response_profile,
+                      User.auto_speech, User.single_message,
+                      User.auto_transcription)
         result = await self.session.execute(stmt)
         return result.mappings().all()
