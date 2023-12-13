6519ea7635467524c539ddf389e974d3ec499a1d
Kostek <ksytnik73kwer@gmail.com>
2023-12-13 18:14:59 +0100

    Update a "get_user_by_id" function for more details and usable.

--- backend/infrastructure/database/repo/users.py
+++ backend/infrastructure/database/repo/users.py
@@ -1,15 +1,29 @@
-from sqlalchemy import select
+from sqlalchemy import select, and_, func, update
+from sqlalchemy.dialects.postgresql import insert
 
-from infrastructure.database.models import User
+from infrastructure.database.models import User, UserContextLimit
 from infrastructure.database.repo.base import BaseRepo
 
 
 class UserRepo(BaseRepo):
-    async def get_user_info(self, offset=None, limit=None) -> list[dict]:
-        stmt = select(User.user_id, User.username,
-                      User.full_name, User.created_at,
-                      User.user_profile, User.response_profile,
-                      User.auto_speech, User.single_message,
-                      User.auto_transcription)
+
+    async def get_user_by_id(self, user_id: int) -> dict:
+        stmt = (
+            select(
+                User.model_id,
+                User.language_id,
+                User.auto_transcription,
+                User.auto_speech,
+                User.single_message,
+                User.user_profile,
+                User.response_profile,
+                func.coalesce(UserContextLimit.window_limit, 500).label("window_limit")).outerjoin(
+                UserContextLimit, and_(UserContextLimit.user_id == User.user_id,
+                                       UserContextLimit.model_id == User.model_id,
+                                       )).where(User.user_id == user_id)
+                )
+
         result = await self.session.execute(stmt)
         return result.mappings().all()
+
+        return result.mappings().first()
